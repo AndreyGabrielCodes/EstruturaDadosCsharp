f8e4076daacbbacc7509fd4668f1afcbda40e0a7
Andrey Gabriel <andreygabriel.scnd@gmail.com>
2025-01-16 13:46:50 -0300

    CREATE | Iniciado resposta do Q3
    - Copiado função de ordenação
    - Criado texto da pergunta em program

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -19,6 +19,14 @@ namespace Arrays
              //ou seja, {1,1,2,3,3,4,5,6} = {2,4,5,6}
             Console.WriteLine("\nQ2 - Primeiros inteiros não repetidos de um Array\n- Input: {-2,11,9,0,7,-5,15,3,1,14,9,-1,6,12,1,4,8,0,-3,10,2,-5,12,14,-1,5,-4}");
             Console.WriteLine($"- Resposta: {Q2.ExibePrimeirosInteiros(arrayAleatorio)}");
+
+
+            //Q3
+            int[] arrayOrdenadoEsquerda = { -5, -2, -1, 0, 1, 1, 3, 6, 7, 9, 9, 11, 12, 14, 15 };
+            int[] arrayOrdenadoDireita = { -5, -4, -3, -1, 0, 2, 4, 5, 8, 10, 12, 14 };
+            Console.WriteLine("\nQ3 - Mescle dois Arrays ordenados\n- Input: {-5,-2,-1,0,1,1,3,6,7,9,9,11,12,14,15} e {-5,-4,-3,-1,0,2,4,5,8,10,12,14}");
+            //Console.WriteLine($"- Resposta: ");
+            Q3.ExibeArraysMesclados(arrayOrdenadoEsquerda, arrayOrdenadoDireita);
         }
     }
 }
--- Arrays/Q3.cs
+++ Arrays/Q3.cs
@@ -8,5 +8,35 @@ namespace Arrays
 {
     public class Q3
     {
+        public static void ExibeArraysMesclados(int[] arrayEsquerda, int[] arrayDireita)
+        {
+            int[] arrayExibicao = MesclaArrays(arrayEsquerda, arrayDireita);
+            
+            foreach(int i in arrayExibicao)
+            {
+                Console.WriteLine(i);
+            }
+        }
+        /*private static int[] MesclaArrays(int[] arrayEsquerda, int[] arrayDireita)
+        {
+            
+        }*/
+
+        private static int[] OrdenaArray(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[j] < array[i])
+                    {
+                        int temp = array[i];
+                        array[i] = array[j];
+                        array[j] = temp;
+                    }
+                }
+            }
+            return array;
+        }
     }
 }
