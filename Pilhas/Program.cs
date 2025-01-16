f1ea07dca483a524e3473387aea2abaed9d73633
Andrey Gabriel <andreygabriel.scnd@gmail.com>
2025-01-16 16:32:02 -0300

    CREATE | Iniciado questões de pilhas
    - Criado classes de cada pergunta
    - Criado texto da Q1

---
+++ Pilhas/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pilhas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Q1
+            Console.WriteLine("Q1 - Calcule o valor de uma expressão pós-fixada usando uma pilha\n- Input: {3 4 + 2 * 5 -}");
+            Console.WriteLine("- Resposta: "); 
+        }
+    }
+}
