d4ba5a53a9061175a944cc35b277834f2d661821
Andrey Gabriel <andreygabriel.scnd@gmail.com>
2025-01-20 17:51:06 -0300

    CREATE | Finalizado Q1
    - Lido as funções do tipo e visto que já há um tipo para inverter os valores
    - Foi feito mais uma função para retirar os valores da coleção e inserir em uma nova lista para entender como funciona

---
+++ ListasVinculadas+/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListasVinculadas_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Q1
+            LinkedList<int> numeros = new LinkedList<int>();
+            int numeroElementos = 10;
+            for(int i = 0; i < numeroElementos; i++)
+            {
+                numeros.AddLast(i);
+            }
+            Console.WriteLine($"Q1 - Ordene inversamente uma lista vinculada\n-Input: ({string.Join(",", numeros)})");
+            Console.WriteLine($"- Resposta: ({Q1.ExibeListaInvertida(numeros)})");
+        }
+    }
+}
---
+++ ListasVinculadas+/Q1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListasVinculadas_
+{
+    public class Q1
+    {
+
+        public static string ExibeListaInvertida(LinkedList<int> numeros)
+        {
+            string listaAlterada = string.Join(",", InverteLista(numeros));
+            return listaAlterada;
+        }
+
+        private static LinkedList<int> InverteLista(LinkedList<int> numeros) 
+        {
+            LinkedList<int> listaAlterada = new LinkedList<int>();
+            foreach (int numero in numeros.Reverse()) {
+                listaAlterada.AddLast(numero);
+            }
+            return listaAlterada;
+        }
+    }
+}
