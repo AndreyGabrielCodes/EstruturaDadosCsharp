f2faab474bfdfe20b5eb4bf09016b34e4b9b8524
Andrey Gabriel <andreygabriel.scnd@gmail.com>
2025-01-21 16:46:46 -0300

    CREATE | Iniciado Q4
    - Criado função que compara o um nó com os próximos e retira quando for duplicado, porém remove incorretamente

--- ListasVinculadas+/Program.cs
+++ ListasVinculadas+/Program.cs
@@ -8,12 +8,8 @@ namespace ListasVinculadas
         static void Main(string[] args)
         {
             //Q1
-            LinkedList<int> numeros = new LinkedList<int>();
-            int numeroElementos = 10;
-            for (int i = 0; i < numeroElementos; i++)
-            {
-                numeros.AddLast(i);
-            }
+            int[] array = { 1, 2, 3, 4, 5, 6, 7 ,8 ,9, 10};
+            LinkedList<int> numeros = new LinkedList<int>(array);
             Console.WriteLine($"Q1 - Ordene inversamente uma lista vinculada\n- Input: ({string.Join(",", numeros)})");
             Console.WriteLine($"- Resposta: ({Q1.ExibeListaInvertida(numeros)})");
 
@@ -26,6 +22,12 @@ namespace ListasVinculadas
             Console.WriteLine($"\nQ3 - Retorne o enésimo nó partindo do final de uma lista vinculada");
             Console.WriteLine($"- Input: {enesimoNo} e ({string.Join(",", numeros)})");
             Console.WriteLine($"- Resposta: {Q3.RetornaValorEnesimoNo(numeros, enesimoNo)}");
+
+            //Q4
+            int[] numerosDuplicados = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 4, 15, 16, 17, 18, 6, 10 };
+            Console.WriteLine($"\nQ4 - Remova elementos duplicados de uma lista vinculada");
+            Console.WriteLine($"- Input: ({string.Join(",", numerosDuplicados)})");
+            Console.WriteLine($"- Resposta: {string.Join(",",Q4.RemoveDuplicados(numeros))}");
         }
     }
 }
---
+++ ListasVinculadas+/Q4.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace ListasVinculadas
+{
+    public class Q4
+    {
+        public static LinkedList<int> RemoveDuplicados(LinkedList<int> lista)
+        {
+            LinkedListNode<int> noPrimario = lista.First;
+
+            while (noPrimario != null)
+            {
+                LinkedListNode<int> noSecundario = lista.First.Next;
+                while (noSecundario != null)
+                {
+                    if (noSecundario.Value == noPrimario.Value)
+                    {
+                        lista.Remove(noPrimario.Value);
+                        lista.Remove(noSecundario.Value);
+                        break;
+                    }
+                    noSecundario = noSecundario.Next;
+                }
+                noPrimario = noPrimario.Next;
+            }
+            return lista;
+        }
+    }
+}
