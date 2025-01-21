38848816127e410b7bd058e3d6417ab37988d92e
Andrey Gabriel <andreygabriel.scnd@gmail.com>
2025-01-21 13:59:42 -0300

    UPDATE | Realizado testes
    - Tentativas de criação de uma lista circular para testar efetividade da função

--- ListasVinculadas+/Program.cs
+++ ListasVinculadas+/Program.cs
@@ -17,13 +17,12 @@ namespace ListasVinculadas
             Console.WriteLine($"Q1 - Ordene inversamente uma lista vinculada\n-Input: ({string.Join(",", numeros)})");
             Console.WriteLine($"- Resposta: ({Q1.ExibeListaInvertida(numeros)})");
 
-
             //Q2
             LinkedList<int> numerosComLoop = numeros;
-            LinkedListNode<int> no = numerosComLoop.Last;
-            //no.Next = numerosComLoop.First;
-            Console.WriteLine($"Q2 - Detecte um loop (laço) em uma lista vinculada");
-            Console.WriteLine($"- Resposta: ({Q2.ExibeNoEncontrado(numeros)})");
+            LinkedListNode<int> primeiroNo = numerosComLoop.First;
+            primeiroNo.Next = numerosComLoop.First;
+            Console.WriteLine($"\nQ2 - Detecte um loop (laço) em uma lista vinculada");
+            Console.WriteLine($"- Resposta: {Q2.ExibeNoEncontrado(numeros)} e {Q2.ExibeNoEncontrado(numerosComLoop)}");
         }
     }
 }
