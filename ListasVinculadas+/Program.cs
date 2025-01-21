276ed9a37311335174e6e446e2d20938f5acfaba
Andrey Gabriel <andreygabriel.scnd@gmail.com>
2025-01-21 16:13:50 -0300

    CREATE | Finalizado Q2 e Q3
    - Finalizado Q2
    - Criado classe para Q3 que primeiro conta a quantidade total de elementos e depois itera até estar no elemento correto

--- ListasVinculadas+/Program.cs
+++ ListasVinculadas+/Program.cs
@@ -14,15 +14,18 @@ namespace ListasVinculadas
             {
                 numeros.AddLast(i);
             }
-            Console.WriteLine($"Q1 - Ordene inversamente uma lista vinculada\n-Input: ({string.Join(",", numeros)})");
+            Console.WriteLine($"Q1 - Ordene inversamente uma lista vinculada\n- Input: ({string.Join(",", numeros)})");
             Console.WriteLine($"- Resposta: ({Q1.ExibeListaInvertida(numeros)})");
 
             //Q2
-            LinkedList<int> numerosComLoop = numeros;
-            LinkedListNode<int> primeiroNo = numerosComLoop.First;
-            primeiroNo.Next = numerosComLoop.First;
             Console.WriteLine($"\nQ2 - Detecte um loop (laço) em uma lista vinculada");
-            Console.WriteLine($"- Resposta: {Q2.ExibeNoEncontrado(numeros)} e {Q2.ExibeNoEncontrado(numerosComLoop)}");
+            Console.WriteLine($"- Resposta: {Q2.ExibeNoEncontrado(numeros)}");
+
+            //Q3
+            int enesimoNo = 9;
+            Console.WriteLine($"\nQ3 - Retorne o enésimo nó partindo do final de uma lista vinculada");
+            Console.WriteLine($"- Input: {enesimoNo} e ({string.Join(",", numeros)})");
+            Console.WriteLine($"- Resposta: {Q3.RetornaValorEnesimoNo(numeros, enesimoNo)}");
         }
     }
 }
---
+++ ListasVinculadas+/Q3.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListasVinculadas
+{
+    public class Q3
+    {
+
+
+        public static string RetornaValorEnesimoNo(LinkedList<int> lista, int enesimoNo)
+        {
+            if (!ValidaValorEnesimoNo(enesimoNo, lista))
+            {
+                return "Valor inserido em Enesimo Nó é maior que o tamanho da lista";
+            }
+            int quantidadeIteracao = CalculaIteracoes(RetornaTamanhoLista(lista), enesimoNo);
+            LinkedListNode<int> no = lista.Last;
+            for (int i = 0; i < quantidadeIteracao; i++)
+            {
+                no = no.Previous;
+            }
+            return no.Value.ToString();
+        }
+
+        private static bool ValidaValorEnesimoNo(int enesimoNo, LinkedList<int> lista)
+        {
+            if (enesimoNo > RetornaTamanhoLista(lista))
+            {
+                return false;
+            }
+            return true;
+        }
+
+        private static int CalculaIteracoes(int tamanhoLista, int enesimoNo)
+        {
+            int quantidadeIteracao = tamanhoLista - enesimoNo;
+            return quantidadeIteracao;
+        }
+
+        private static int RetornaTamanhoLista(LinkedList<int> lista)
+        {
+            LinkedListNode<int> no = lista.Last;
+            int tamanhoLista = 0;
+            while (no != null)
+            {
+                no = no.Previous;
+                tamanhoLista++;
+            }
+            return tamanhoLista;
+        }
+    }
+}
