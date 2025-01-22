075aafb83730eb6262f177905152fa8b5b0c5b81
Andrey Gabriel <andreygabriel.scnd@gmail.com>
2025-01-22 09:04:43 -0300

    UPDATE | Corrigido erro em Q1
    - Identificado que a resposta que indicava os pares simetricos estava duplicando os resultados
    - Criado função que verifica se os pares encontrados já foram encontrados anteriormente

--- Dicionarios/Q1.cs
+++ Dicionarios/Q1.cs
@@ -25,19 +25,37 @@ namespace Dicionarios
             Dictionary<string, int[]> paresSimetricos = new Dictionary<string, int[]>();
             int tamanho = array.Length;
             int numeroParEncontrado = 0;
+            List<int> simetricosEncontrados = new List<int>();
             for (int i = 0; i < tamanho; i++)
             {
                 for (int j = 0; j < tamanho; j++)
                 {
                     if (array[i] == array[j] && i != j)
                     {
-                        numeroParEncontrado++;
-                        int[] paresTemp = {array[i], array[j]};
-                        paresSimetricos.Add($"Par {numeroParEncontrado}", paresTemp);
+                        if (ValidaParEncontrado(simetricosEncontrados, array[i], array[j]))
+                        {
+                            numeroParEncontrado++;
+                            simetricosEncontrados.Add(array[i]);
+                            int[] paresTemp = { array[i], array[j] };
+                            paresSimetricos.Add($"Par {numeroParEncontrado}", paresTemp);
+                        }
                     }
                 }
             }
             return paresSimetricos;
+        }
+
+        private static bool ValidaParEncontrado(List<int> simetricosEncontrados, int par1, int par2)
+        {
+            bool parValido = true;
+            foreach (int parEncontrado in simetricosEncontrados)
+            {
+                if (par1 == parEncontrado || par2 == parEncontrado)
+                {
+                    parValido = false;
+                }
+            }
+            return parValido;
         }
     }
 }
