3c934275e1f9b2c334b919c446acc5a34da7fbcc
Andrey Gabriel <andreygabriel.scnd@gmail.com>
2025-01-22 06:57:30 -0300

    CREATE | Criado projeto de dicionario e finalizado Q1
    - Criado projeto de dicionarios com classe Q1
    - Criado função que percorre todos os itens e os compara, após os salva em um dicionário

---
+++ Dicionarios/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dicionarios
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Q1
+            int[] arraySimetrico = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 2, 13, 4, 15, 16, 17, 18, 6, 10 };
+            Console.WriteLine($"Q1 - Encontre pares simétricos em um Array\n- Input: ({string.Join(",",arraySimetrico)})");
+            Console.WriteLine($"- Resposta: {Q1.ExibeParesEncontrados(arraySimetrico)}");
+
+        }
+    }
+}
---
+++ Dicionarios/Q1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dicionarios
+{
+    public class Q1
+    {
+        public static string ExibeParesEncontrados(int[] array)
+        {
+            Dictionary<string, int[]> paresSimetricos = new Dictionary<string, int[]>();
+            paresSimetricos = EncontraPares(array);
+            string texto = "";
+            foreach (KeyValuePair<string, int[]> item in paresSimetricos)
+            {
+                texto += item.Key + ": (" +string.Join(",", item.Value) + "), ";
+            }
+            return texto;
+        }
+
+        private static Dictionary<string, int[]> EncontraPares(int[] array)
+        {
+            Dictionary<string, int[]> paresSimetricos = new Dictionary<string, int[]>();
+            int tamanho = array.Length;
+            int numeroParEncontrado = 0;
+            for (int i = 0; i < tamanho; i++)
+            {
+                for (int j = 0; j < tamanho; j++)
+                {
+                    if (array[i] == array[j] && i != j)
+                    {
+                        numeroParEncontrado++;
+                        int[] paresTemp = {array[i], array[j]};
+                        paresSimetricos.Add($"Par {numeroParEncontrado}", paresTemp);
+                    }
+                }
+            }
+            return paresSimetricos;
+        }
+    }
+}
