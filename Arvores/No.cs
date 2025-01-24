6e442c50f2edc6ed5cbabccb4afe901265c04b7d
Andrey Gabriel <andreygabriel.scnd@gmail.com>
2025-01-23 21:00:04 -0300

    CREATE | Iniciado Arvores
    - Criado classe para a árvore

---
+++ Arvores/No.cs
@@ -0,0 +1,16 @@
+﻿namespace Arvores
+{
+    public class No
+    {
+        public int valor { get; set; }
+        public No direita { get; set; }
+        public No esquerda { get; set; }
+
+        public No(int valor)
+        {
+            this.valor = valor;
+            direita = null;
+            esquerda = null;
+        }
+    }
+}
