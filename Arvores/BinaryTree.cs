e4e7682f9646e69619bfd6cd191ef828b9ce12fc
Andrey Gabriel <andreygabriel.scnd@gmail.com>
2025-01-24 17:58:34 -0300

    CREATE | Criado arvore
    - Excluido classe anterior do no
    - Criado nova classe para o no
    - Criado nova classe para a arvore
    - Inicializado valores e feito teste de impressão

---
+++ Arvores/BinaryTree.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Arvores
+{
+    public class BinaryTree<T>
+    {
+        public TreeNode<T> Root { get; private set; }
+
+        public BinaryTree()
+        {
+            Root = null;
+        }
+
+        public void Insert(T value)
+        {
+            Root = InsertRecursive(Root, value);
+        }
+
+        private TreeNode<T> InsertRecursive(TreeNode<T> node, T value)
+        {
+            if (node == null)
+            {
+                return new TreeNode<T>(value);
+            }
+
+            if (Comparer<T>.Default.Compare(value, node.Value) < 0)
+            {
+                node.Left = InsertRecursive(node.Left, value);
+            }
+            else if (Comparer<T>.Default.Compare(value, node.Value) > 0)
+            {
+                node.Right = InsertRecursive(node.Right, value);
+            }
+
+            return node;
+        }
+
+        public bool Search(T value)
+        {
+            return SearchRecursive(Root, value);
+        }
+
+        private bool SearchRecursive(TreeNode<T> node, T value)
+        {
+            if (node == null)
+            {
+                return false;
+            }
+
+            if (EqualityComparer<T>.Default.Equals(node.Value, value))
+            {
+                return true;
+            }
+            else if (Comparer<T>.Default.Compare(value, node.Value) < 0)
+            {
+                return SearchRecursive(node.Left, value);
+            }
+            else
+            {
+                return SearchRecursive(node.Right, value);
+            }
+        }
+
+        public void InOrderTraversal(Action<T> action)
+        {
+            InOrderTraversalRecursive(Root, action);
+        }
+
+        private void InOrderTraversalRecursive(TreeNode<T> node, Action<T> action)
+        {
+            if (node != null)
+            {
+                InOrderTraversalRecursive(node.Left, action);
+                action.Invoke(node.Value);
+                InOrderTraversalRecursive(node.Right, action);
+            }
+        }
+
+    }
+}
--- Arvores/Program.cs
+++ Arvores/Program.cs
@@ -1,15 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Arvores
 {
     internal class Program
     {
         static void Main(string[] args)
-        { 
+        {
+            //Inicialização de uma arvore com 15 valores aleatórios não repetidos
+            BinaryTree<int> arvore = new BinaryTree<int>();
+            arvore.Insert(6);
+            arvore.Insert(1);
+            arvore.Insert(3);
+            arvore.Insert(7);
+            arvore.Insert(9);
+            arvore.Insert(5);
+            arvore.Insert(2);
+            arvore.Insert(10);
+            Action<int> printTeste = value => Console.WriteLine(value);
+            arvore.InOrderTraversal(printTeste);
         }
+
     }
 }
